aaae01d60614e6712187cb89b9da21f4e09f5a09
vaishnaviteli210-cyber <vaishnaviteli210@gmail.com>
2026-08-12 12:44:18 +0530

    DATABASE file

---
+++ SRC/config.py
@@ -0,0 +1,9 @@
+
+#DATABASE
+
+DB_HOST = "localhost"
+DB_PORT = 3306
+DB_NAME = "attendance"
+DB_USER = "root"
+DB_PASSWORD = "root123"
+
---
+++ SRC/information_of_project.py
@@ -0,0 +1,8 @@
+#FOR DATABASE FILE 
+'''
+
+     Use before database.py download the  tabulate
+     This command : pip install tabulate
+
+     
+'''
